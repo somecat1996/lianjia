1d9cfe4e3be04829fbd3da8dce9f73491ccf1c44
somecat1996 <369150573@qq.com>
2018-06-23 14:13:26 +0800

    add comments

--- Spider.py
+++ Spider.py
@@ -1,13 +1,15 @@
 # coding=utf8
+# 用于爬取房源信息
 import requests
 from bs4 import BeautifulSoup
-import csv
 import os
 import multiprocessing
 import json
 import re
 
-
+# 爬虫函数，爬取单个区域所有房源信息
+# areaName 区域名称
+# url 区域url
 def Spider(areaName, url):
     host = "https://sh.lianjia.com"
     # labels = ['ID', '标题', '副标题', '总价', '总价单位', '均价', '小区名称', '所在区域', '房屋户型', '所在楼层', '建筑面积', '户型结构', '套内面积', '建筑类型',
@@ -20,21 +22,29 @@ def Spider(areaName, url):
     # file.close()
 
     saUrl = url
+    # 打开当前区域列表
     tmpMain = requests.get(host + saUrl).text
     tmpMainSoup = BeautifulSoup(tmpMain)
+    # 获得总页数total
     total = eval(tmpMainSoup.select(
         'div[class="page-box house-lst-page-box"]'
     )[0].get("page-data"))['totalPage']
+
     print("Reading %s" % areaName)
+    # 打开所写入的文件
     with open('data/' + areaName + '.json', 'w') as File:
         houses = []
+        # 遍历列表每一页
         for i in range(1, total + 1):
             print("area", areaName, "page", str(i), "in", str(total))
             # writer = csv.writer(csvFile)
             listPageUrl = host + saUrl + "/pg%d/" % i
+            # 获得网页html
             listPage = BeautifulSoup(requests.get(listPageUrl).text)
+            # 获得本页中所有房源详细信息页面url
             singlePageUrls = listPage.select('div[class="title"] > a')
             for j in singlePageUrls:
+                # 爬取网页中所有信息
                 house = {}
                 url = j.get('href')
                 house['ID'] = re.findall('https://sh.lianjia.com/ershoufang/(\d*).html', url)[0]
@@ -49,7 +59,7 @@ def Spider(areaName, url):
                 house["小区名称"] = overview.select('div[class="communityName"] > a[target="_blank"]')[0].string
                 house["所在区域"] = overview.select('div[class="areaName"] > span[class="info"]')[0].text.replace('\xa0',
                                                                                                               ' ')
-
+                # 基本信息
                 base = singlePage.select('div[class="base"] > div[class="content"] > ul > li')
                 for node in base:
                     name, value = node.contents
@@ -58,6 +68,7 @@ def Spider(areaName, url):
                     #     labels.append(name)
                     house[name] = value
 
+                # 交易信息
                 transaction = singlePage.select('div[class="transaction"] > div[class="content"] > ul > li')
                 for node in transaction:
                     name, value = node.select('span')
@@ -66,6 +77,7 @@ def Spider(areaName, url):
                     #     labels.append(name)
                     house[name] = value
 
+                # 房源优势
                 features = singlePage.find_all('div', class_=["tags", "baseattribute"])
                 for node in features:
                     name = node.select('div[class="name"]')[0].string
@@ -81,6 +93,7 @@ def Spider(areaName, url):
                 #     else:
                 #         writeRow.append('')
                 houses.append(house)
+        # 写入文件
         json.dump(houses, File)
 
 
@@ -100,12 +113,12 @@ if __name__ == '__main__':
     urlFile = open('url.txt', 'r')
     smallAreas = json.load(urlFile)
     urlFile.close()
-    Spider("虹桥", smallAreas["虹桥"])
+    # Spider("虹桥", smallAreas["虹桥"])
 
-    # p = multiprocessing.Pool()
-    # for a in smallAreas:
-    #     if a not in exist:
-    #         p.apply_async(Spider, args=(a, smallAreas[a]))
-    # p.close()
-    # p.join()
+    p = multiprocessing.Pool()
+    for a in smallAreas:
+        if a not in exist:
+            p.apply_async(Spider, args=(a, smallAreas[a]))
+    p.close()
+    p.join()
     print('finish')
